aca72ae39d07fd7c21b0762aea43f3ea2d881b99
230442 Michal Augustyniak <230442@edu.p.lodz.pl>
2021-04-16 01:06:00 +0200

    Added JavaFX configuration

---
+++ sudoku/src/main/java/compprog/sudoku/JavaFx.java
@@ -0,0 +1,42 @@
+package compprog.sudoku;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class JavaFx extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        SudokuSolver sudokuSolver = new BacktrackingSudokuSolver();
+        SudokuBoard board = new SudokuBoard(sudokuSolver);
+        board.solveGame();
+        launch(args);
+    }
+
+}
--- sudoku/src/main/java/compprog/sudoku/MainClass.java
+++
@@ -1,9 +0,0 @@
-package compprog.sudoku;
-
-public class MainClass {
-    public static void main(String[] args) {
-        SudokuSolver sudokuSolver = new BacktrackingSudokuSolver();
-        SudokuBoard board = new SudokuBoard(sudokuSolver); 
-        board.solveGame();
-    }
-}
