51b61da52f413ebc3fab8e259fe7aa0bc4c0473c
230442 Michal Augustyniak <230442@edu.p.lodz.pl>
2021-05-18 13:28:44 +0200

    Dao method close() implemented properly

--- sudoku/Model/src/main/java/compprog/sudoku/Dao.java
+++ sudoku/Model/src/main/java/compprog/sudoku/Dao.java
@@ -2,10 +2,12 @@ package compprog.sudoku;
 
 import java.io.IOException;
 
-public interface Dao<T> {
+public interface Dao<T> extends AutoCloseable {
 
     T read() throws IOException, ClassNotFoundException;
 
     void write(T obj) throws IOException;
 
+    @Override
+    void close() throws IOException;
 }
--- sudoku/Model/src/main/java/compprog/sudoku/FileSudokuBoardDao.java
+++ sudoku/Model/src/main/java/compprog/sudoku/FileSudokuBoardDao.java
@@ -8,6 +8,8 @@ import java.io.ObjectOutputStream;
 
 public class FileSudokuBoardDao<SudokuBoard> implements Dao<SudokuBoard>, AutoCloseable {
     private String filename;
+    private ObjectOutputStream writer;
+    private ObjectInputStream reader;
 
     public FileSudokuBoardDao(String filename) {
         this.filename = filename;
@@ -15,20 +17,23 @@ public class FileSudokuBoardDao<SudokuBoard> implements Dao<SudokuBoard>, AutoCl
 
     @Override
     public void write(SudokuBoard obj) throws IOException {
-        try (ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(filename))) {
-            writer.writeObject(obj);
-        }
+        writer = new ObjectOutputStream(new FileOutputStream(filename));
+        writer.writeObject(obj);
     }
 
     @Override
     public SudokuBoard read() throws IOException, ClassNotFoundException {
-        try (ObjectInputStream reader = new ObjectInputStream(new FileInputStream(filename))) {
-            return (SudokuBoard) reader.readObject();
-        }
+        reader = new ObjectInputStream(new FileInputStream(filename));
+        return (SudokuBoard) reader.readObject();
     }
 
     @Override
-    public void close() {
-        System.out.println("Closing");
+    public void close() throws IOException {
+        if (writer != null) {
+            writer.close();
+        }
+        if (reader != null) {
+            reader.close();
+        }
     }
 }
